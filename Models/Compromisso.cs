3bea31e52970e294e56900c2253338e58b17ca77
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-08-08 18:24:41 -0400

    Ajustes nas páginas

--- Models/Compromisso.cs
+++ Models/Compromisso.cs
@@ -5,7 +5,6 @@ namespace ProAspNetCoreMvcValidation.Models
     public class Compromisso
     {
         public string NomeCliente { get; set; }
-        //[UIHint("Date")]
         public DateTime Data { get; set; }
         public bool AceitaTermos { get; set; }
     }
